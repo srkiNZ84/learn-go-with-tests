4a97edf9f33ad855979ddb1e6df3b51b63990ffa
Srđan Đukić <srd@mega.co.nz>
2022-04-19 12:06:31 +1200

    Add FileSystem store code and tests
    Switch the main program to use the new FileSystem (persistent) based
    store and ensure we have tests for the new store type.
    
    Issue(s): None

--- build-an-app-1-http/main.go
+++ build-an-app-1-http/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	store := NewInMemoryPlayerStore()
+	//store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening db %s, %v", dbFileName, err)
+	}
+	store := &FileSystemPlayerStore{db}
 	server := NewPlayerServer(store)
-	log.Fatal(http.ListenAndServe(":5000", server))
+	
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
